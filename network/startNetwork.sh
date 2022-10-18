2715461e499581eaa565ea521710229ed339a148
Minh Dinh <minhdinh@Megan3koPC.localdomain>
2022-10-18 20:49:03 +0700

    Network set up with 3 orgs and chaincode is deployed on both three peers

---
+++ network/startNetwork.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error
+set -e
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+starttime=$(date +%s)
+
+##install node packages
+#pushd ./chaincode-supply/supply/
+#npm install
+#popd
+
+# clean out any old identites in the wallets
+#rm -rf javascript/wallet/*
+#rm -rf java/wallet/*
+#rm -rf typescript/wallet/*
+#rm -rf go/wallet/*
+
+# launch network; create channel and join peer to channel
+pushd ./test-network
+./network.sh down
+./network.sh up createChannel -ca -s couchdb -c mychannel
+#./network.sh deployCC -ccn fabcar -ccv 1 -cci initLedger -ccl ${CC_SRC_LANGUAGE} -ccp ${CC_SRC_PATH}
+popd
+pushd ./test-network/addOrg3
+./addOrg3.sh up -s couchdb -c mychannel
+popd
+
+##Deploy chaincode on org1, org2
+pushd ./test-network
+./network.sh deployCC -ccn supply -ccp ../chaincode-supply/supply/ -ccl javascript -cci initLedger -c mychannel
+popd
+
+##export path for org3
+export PATH=${PWD}/bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/config/
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org3MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/test-network/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/test-network/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+export CORE_PEER_ADDRESS=localhost:11051
+
+#package chaincode
+peer lifecycle chaincode package supply.tar.gz --path ./chaincode-supply/supply/ --lang node --label supply_1.0
+
+#install
+peer lifecycle chaincode install supply.tar.gz
+
+#get the package identifier
+peer lifecycle chaincode queryinstalled
+
+#approve chaincode
+export PACKAGE_ID=$(peer lifecycle chaincode calculatepackageid supply.tar.gz)
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --channelID mychannel --name supply --version 1.0 --package-id $PACKAGE_ID --sequence 1 --init-required
+
+#commit query
+peer lifecycle chaincode querycommitted --channelID mychannel --name supply  --cafile "${PWD}/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+
+#init
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n supply --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/test-network/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" --peerAddresses localhost:11051 --tlsRootCertFiles "${PWD}/test-network/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" -c '{"function":"initLedger", "Args":[]}'
+
+cat <<EOF
+
+Total setup execution time : $(($(date +%s) - starttime)) secs ...
+
+Next, use the FabCar applications to interact with the deployed FabCar contract.
+The FabCar applications are available in multiple programming languages.
+Follow the instructions for the programming language of your choice:
+
+JavaScript:
+
+  Start by changing into the "javascript" directory:
+    cd javascript
+
+  Next, install all required packages:
+    npm install
+
+  Then run the following applications to enroll the admin user, and register a new user
+  called appUser which will be used by the other applications to interact with the deployed
+  FabCar contract:
+    node enrollAdmin
+    node registerUser
+
+  You can run the invoke application as follows. By default, the invoke application will
+  create a new car, but you can update the application to submit other transactions:
+    node invoke
+
+  You can run the query application as follows. By default, the query application will
+  return all cars, but you can update the application to evaluate other transactions:
+    node query
+
+TypeScript:
+
+  Start by changing into the "typescript" directory:
+    cd typescript
+
+  Next, install all required packages:
+    npm install
+
+  Next, compile the TypeScript code into JavaScript:
+    npm run build
+
+  Then run the following applications to enroll the admin user, and register a new user
+  called appUser which will be used by the other applications to interact with the deployed
+  FabCar contract:
+    node dist/enrollAdmin
+    node dist/registerUser
+
+  You can run the invoke application as follows. By default, the invoke application will
+  create a new car, but you can update the application to submit other transactions:
+    node dist/invoke
+
+  You can run the query application as follows. By default, the query application will
+  return all cars, but you can update the application to evaluate other transactions:
+    node dist/query
+
+Java:
+
+  Start by changing into the "java" directory:
+    cd java
+
+  Then, install dependencies and run the test using:
+    mvn test
+
+  The test will invoke the sample client app which perform the following:
+    - Enroll admin and appUser and import them into the wallet (if they don't already exist there)
+    - Submit a transaction to create a new car
+    - Evaluate a transaction (query) to return details of this car
+    - Submit a transaction to change the owner of this car
+    - Evaluate a transaction (query) to return the updated details of this car
+
+Go:
+
+  Start by changing into the "go" directory:
+    cd go
+
+  Then, install dependencies and run the test using:
+    go run fabcar.go
+
+  The test will invoke the sample client app which perform the following:
+    - Import user credentials into the wallet (if they don't already exist there)
+    - Submit a transaction to create a new car
+    - Evaluate a transaction (query) to return details of this car
+    - Submit a transaction to change the owner of this car
+    - Evaluate a transaction (query) to return the updated details of this car
+
+EOF
