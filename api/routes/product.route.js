9ce53c011af3db6a5c96c7676b4fedf76561c3fe
huynhanh475 <huynhanh475@gmail.com>
2022-10-20 09:33:00 +0700

    Add roles constant

--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { send } from "../utils/api-response.js";
+import { roles } from "../utils/constants.js";
 import canAccess from "../middlewares/can-access.js"
 
 const productRouter = Router();
 
-productRouter.post('/', canAccess(["admin", "manufacturer"]), (req, res) => {
+productRouter.post('/', canAccess([roles.admin, roles.manufacturer]), (req, res) => {
     
     return send(res, {
         status: 200,
