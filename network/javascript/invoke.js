601f9d96297759fc86c1f0a8e7319e61b08771a8
MinhDinh-4869 <minhdeptrai4869@gmail.com>
2022-10-20 10:24:01 +0700

    modify initLedger contract, add queryProduct contract

--- network/javascript/invoke.js
+++ network/javascript/invoke.js
@@ -42,7 +42,8 @@ async function main() {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR12', 'Dave')
-        await contract.submitTransaction('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom');
+        //await contract.submitTransaction('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom');
+        await contract.submitTransaction('queryProduct', 'product0');
         console.log('Transaction has been submitted');
 
         // Disconnect from the gateway.
