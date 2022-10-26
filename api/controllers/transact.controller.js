ba61f42f720da05a0787f2ec3a5fe741552ccf42
Hui <howardsu08@gmail.com>
2022-10-26 09:49:58 +0700

    fix apis

--- api/controllers/transact.controller.js
+++ api/controllers/transact.controller.js
@@ -3,121 +3,81 @@ import { badRequest, send } from '../utils/api-response.js';
 import { roles } from '../utils/constants.js';
 
 export async function registerOrder(req, res) {
-    const { productID, manufacturerID, retailerID, quantity } = req.body;
-    const { role } = req.params;
+    const { userType, productID, manufacturerID, retailerID, quantity } = req.body;
 
     if (!productID || !manufacturerID || !retailerID || !quantity)
         return badRequest(res);
 
-    const isManufacturer = role === roles.manufacturer;
-    const isDeliverer = role === roles.deliverer;
-    const isRetailer = role === roles.retailer;
-
-    const modelRes = await model.registerOrder(isManufacturer, isDeliverer, isRetailer, { productID, manufacturerID, retailerID, quantity });
+    const modelRes = await model.registerOrder(userType, { productID, manufacturerID, retailerID, quantity });
     return send(res, modelRes);
 }
 
 export async function approveOrder(req, res) {
-    const { batchID } = req.body;
-    const { role } = req.params;
+    const { userType, userID, batchID } = req.body;
 
     if (!batchID)
         return badRequest(res);
 
-    const isManufacturer = role === roles.manufacturer;
-    const isDeliverer = role === roles.deliverer;
-    const isRetailer = role === roles.retailer;
-
-    const modelRes = await model.approveOrder(isManufacturer, isDeliverer, isRetailer, { batchID });
+    const modelRes = await model.approveOrder(userType, { userID, batchID });
     return send(res, modelRes);
 }
 
 export async function inviteDeliverer(req, res) {
-    const { batchID, delivererID } = req.body;
-    const { role } = req.params;
+    const { userType, userID, batchID, delivererID } = req.body;
 
     if (!batchID || !delivererID)
         return badRequest(res);
 
-    const isManufacturer = role === roles.manufacturer;
-    const isDeliverer = role === roles.deliverer;
-    const isRetailer = role === roles.retailer;
-
-    const modelRes = await model.inviteDeliverer(isManufacturer, isDeliverer, isRetailer, { batchID, delivererID });
+    const modelRes = await model.inviteDeliverer(userType, { userID, batchID, delivererID });
     return send(res, modelRes);
 }
 
 export async function replyInvitation(req, res) {
-    const { batchID, action } = req.body;
-    const { role } = req.params;
+    const { userType, userID,  batchID, action } = req.body;
 
     if (!batchID || !action)
         return badRequest(res);
 
-    const isManufacturer = role === roles.manufacturer;
-    const isDeliverer = role === roles.deliverer;
-    const isRetailer = role === roles.retailer;
-
-    const modelRes = await model.replyInvitation(isManufacturer, isDeliverer, isRetailer, { batchID, action });
+    const modelRes = await model.replyInvitation(userType, { userID, batchID, action });
     return send(res, modelRes);
 }
 
 export async function transferToDeliverer(req, res) {
-    const { batchID } = req.body;
-    const { role } = req.params;
+    const { userType, userID, batchID } = req.body;
 
     if (!batchID)
         return badRequest(res);
 
-    const isManufacturer = role === roles.manufacturer;
-    const isDeliverer = role === roles.deliverer;
-    const isRetailer = role === roles.retailer;
-
-    const modelRes = await model.transferToDeliverer(isManufacturer, isDeliverer, isRetailer, { batchID });
+    const modelRes = await model.transferToDeliverer(userType, { userID, batchID });
     return send(res, modelRes);
 }
 
 export async function confirmTransfer(req, res) {
-    const { batchID } = req.body;
-    const { role } = req.params;
+    const { userType, userID, batchID } = req.body;
 
     if (!batchID)
         return badRequest(res);
 
-    const isManufacturer = role === roles.manufacturer;
-    const isDeliverer = role === roles.deliverer;
-    const isRetailer = role === roles.retailer;
-
-    const modelRes = await model.confirmTransfer(isManufacturer, isDeliverer, isRetailer, { batchID });
+    const modelRes = await model.confirmTransfer(userType, { userID, batchID });
     return send(res, modelRes);
 }
 
 export async function transferToRetailer(req, res) {
-    const { batchID } = req.body;
-    const { role } = req.params;
+    const { userType, userID, batchID } = req.body;
 
     if (!batchID)
         return badRequest(res);
 
-    const isManufacturer = role === roles.manufacturer;
-    const isDeliverer = role === roles.deliverer;
-    const isRetailer = role === roles.retailer;
-
-    const modelRes = await model.transferToRetailer(isManufacturer, isDeliverer, isRetailer, { batchID });
+    const modelRes = await model.transferToRetailer(userType, { userID, batchID });
     return send(res, modelRes);
 }
 
 export async function receiveProduct(req, res) {
-    const { batchID } = req.body;
-    const { role } = req.params;
+    const { userType, userID, batchID } = req.body;
 
     if (!batchID)
         return badRequest(res);
 
-    const isManufacturer = role === roles.manufacturer;
-    const isDeliverer = role === roles.deliverer;
-    const isRetailer = role === roles.retailer;
-
-    const modelRes = await model.receiveProduct(isManufacturer, isDeliverer, isRetailer, { batchID });
+    const modelRes = await model.receiveProduct(userType, { userID, batchID });
     return send(res, modelRes);
 }
