9c017aad214704340fc773b8467535b2fd0d9883
Hui <howardsu08@gmail.com>
2022-10-31 07:57:19 +0700

    add apis to fe

--- frontend/src/components/Retailer/ProductList/ProductList.js
+++ frontend/src/components/Retailer/ProductList/ProductList.js
@@ -1,55 +1,74 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './ProductList.css';
-import record from './MOCK_DATA (5).json';
 import { DataGrid } from "@mui/x-data-grid";
 import { ProductColumn } from "./ProductColumn";
 import RegisterProductForm from './RegisterProductForm';
+import { request } from '../../../utils/request';
 
 function ProductList() {
-    const data = record;
-    const [isRegister, setIsRegister] = useState(false);
-    const [productID, setProductID] = useState("");
-    const [name, setName] = useState("");
-    const [manufacturerID, setManufacturerID] = useState("");
-    const [date, setDate] = useState("");
-    const [price, setPrice] = useState("");
+  const [data, setData] = useState([]);
+  const [isRegister, setIsRegister] = useState(false);
+  const [productId, setProductID] = useState("");
+  const [name, setName] = useState("");
+  const [manufacturerId, setManufacturerID] = useState("");
+  const [date, setDate] = useState("");
+  const [price, setPrice] = useState("");
 
-    const handleOnClickRegister=(a) => {
-        setIsRegister(true);
-        setProductID(a.productID);
-        setName(a.name);
-        setManufacturerID(a.manufacturerID);
-        setDate(a.date);
-        setPrice(a.price);
-    }
+  const token = localStorage.getItem("AUTH_DATA");
+  const params = {
+    method: "GET",
+    url: "/product",
+    headers: { 'Content-Type': "application/json", 'x-access-token': token },
+  }
 
-    const actionColumn = [
-        {
-          field: "action",
-          headerName: "Action",
-          width: 200,
-          renderCell: (params) => {
-            return (
-              <div className="cellAction">
-                <div className="registerButton" onClick={()=>handleOnClickRegister(params.row)}>Register</div>
-              </div>
-            );
-          },
-        },
-    ];
+  useEffect(() => {
+    const getAllProducts = async () => {
+      const response = await request(params);
+      let rawData = await response.text();
+      let jsonData = JSON.parse(rawData);
+      setData(jsonData["data"]);
+    };
+    getAllProducts();
+    return () => { };
+  }, []);
+
+
+  const handleOnClickRegister = (a) => {
+    setIsRegister(true);
+    setProductID(a.productId);
+    setName(a.name);
+    setManufacturerID(a.manufacturerId);
+    setDate(a.date);
+    setPrice(a.price);
+  }
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <div className="registerButton" onClick={() => handleOnClickRegister(params.row)}>Register</div>
+          </div>
+        );
+      },
+    },
+  ];
   return (
     <div>
-        <RegisterProductForm isRegister={isRegister} setIsRegister={setIsRegister} productID={productID} name={name} manufacturerID={manufacturerID} date={date} price={price}/>
-        <div className="productlisttable">
-            <DataGrid
-            rows={data}
-            columns={ProductColumn.concat(actionColumn)}
-            getRowId={(row) => row.productID+row.manufacturerID}
-            pageSize={9}
-            rowsPerPageOptions={[9]}
-            checkboxSelection
-            />
-        </div>
+      <RegisterProductForm isRegister={isRegister} setIsRegister={setIsRegister} productID={productId} name={name} manufacturerID={manufacturerId} date={date} price={price} />
+      <div className="productlisttable">
+        <DataGrid
+          rows={data}
+          columns={ProductColumn.concat(actionColumn)}
+          getRowId={(row) => row.productId + row.manufacturerId}
+          pageSize={9}
+          rowsPerPageOptions={[9]}
+          checkboxSelection
+        />
+      </div>
 
     </div>
   )
--- frontend/src/components/Retailer/ProductList/RegisterProductForm.js
+++ frontend/src/components/Retailer/ProductList/RegisterProductForm.js
@@ -1,54 +1,74 @@
-import React, {useRef, useState} from 'react';
-import {Modal} from 'antd';
+import React, { useRef, useState } from 'react';
+import { Modal } from 'antd';
 import './ProductList.css';
 
-function RegisterProductForm({isRegister, setIsRegister, productID, name, manufacturerID, date, price}) {
+function RegisterProductForm({ isRegister, setIsRegister, productID, name, manufacturerID, date, price }) {
     const form = useRef();
     const [quantity, setQuantity] = useState("");
-    const handleOk = () => {
+
+    useEffect(() => {
+        const registerOrder = async () => {
+          let rawData = await response.text();
+          let jsonData = JSON.parse(rawData);
+          setData(jsonData["data"]);
+        };
+        registerOrder();
+        return () => { };
+      }, []);
+
+    const handleOk = async (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem("AUTH_DATA");
+        const item = { "productId": productID, "quantity": quantity };
+        const params = {
+            method: "POST",
+            url: "/transact/resigerOrder",
+            body: item,
+            headers: { 'Content-Type': "application/json", 'x-access-token': token },
+        }
+        const response = await request(params);
+
         setIsRegister(false);
         document.getElementById("CreateProductForm").reset();
     };
-    
-      const handleCancel = () => {
+
+    const handleCancel = () => {
         setIsRegister(false);
         document.getElementById("CreateProductForm").reset();
-      };
-  return (
-    <div>
-        <Modal open={isRegister} onOk={handleOk} onCancel={handleCancel} title="Create Product Form">
-            <form ref={form} id="CreateProductForm" className="create_form">
-                <div>
-                    <label for='productID'>1. Product ID: {productID}</label>
-                </div>
+    };
+    return (
+        <div>
+            <Modal open={isRegister} onOk={handleOk} onCancel={handleCancel} title="Create Product Form">
+                <form ref={form} id="CreateProductForm" className="create_form">
+                    <div>
+                        <label for='productID'>1. Product ID: {productID}</label>
+                    </div>
 
-                <div>
-                    <label for='name'>2. Name: {name}</label>
-                </div>
+                    <div>
+                        <label for='name'>2. Name: {name}</label>
+                    </div>
 
-                <div>
-                    <label for='manufacturerID'>3. Manufacturer ID: {manufacturerID}</label>
-                </div>
+                    <div>
+                        <label for='manufacturerID'>3. Manufacturer ID: {manufacturerID}</label>
+                    </div>
 
-                <div>
-                    <label for='date'>4. Date: {date}</label>
-                </div>
+                    <div>
+                        <label for='date'>4. Date: {date}</label>
+                    </div>
 
-                <div>
-                    <label for='price'>5. Price: {price}</label>
-                </div>
+                    <div>
+                        <label for='price'>5. Price: {price}</label>
+                    </div>
 
-                <div>
-                    <label for='quantity'>6. Quantity</label>
-                    <input type="text" name="quantity" onChange={(e)=>{setQuantity(e.target.value)}} required/>
-                </div>
+                    <div>
+                        <label for='quantity'>6. Quantity</label>
+                        <input type="text" name="quantity" onChange={(e) => { setQuantity(e.target.value) }} required />
+                    </div>
 
-            </form>
-            
-        </Modal>
-
-    </div>
-  )
+                </form>
+            </Modal>
+        </div>
+    )
 }
 
 export default RegisterProductForm
