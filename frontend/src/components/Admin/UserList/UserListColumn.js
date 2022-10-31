9d59e2f9460e845e8e706ae24204766899982bdb
Avocado0602 <tainguyenavo06022001@gmail.com>
2022-10-31 23:42:46 +0700

    add user of type fixed and display userId rid in userlist

--- frontend/src/components/Admin/UserList/UserListColumn.js
+++ frontend/src/components/Admin/UserList/UserListColumn.js
@@ -1,5 +1,6 @@
 export const UserListColumn = [
-    { field: "name", headerName: "Name", width: 400,},
-    { field: "email", headerName: "Email", width: 400,},
-    { field: "address", headerName: "Address", width: 400,},
+    { field: "name", headerName: "Name", width: 300,},
+    { field: "userId", headerName: "Manufacturer ID", width: 300,},
+    { field: "email", headerName: "Email", width: 300,},
+    { field: "address", headerName: "Address", width: 300,},
 ];
