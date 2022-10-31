6b2f13a806db95374c9a661d1cfd881117ad334c
Avocado0602 <tainguyenavo06022001@gmail.com>
2022-11-01 02:38:50 +0700

    fix as tuyet anh request for batch journey 2

--- frontend/src/components/Deliverer/TransferList/TransferListColumn.js
+++ frontend/src/components/Deliverer/TransferList/TransferListColumn.js
@@ -1,9 +1,9 @@
 export const TransferListColumn = [
-    { field: "batchId", headerName: "Batch ID", width: 250 },
-    { field: "productId", headerName: "Product ID", width: 250 },
-    { field: "manufacturerId", headerName: "Manufacturer ID", width: 250 },
-    { field: "retailerId", headerName: "Retailer ID", width: 250 },
+    { field: "batchId", headerName: "Batch ID", width: 150 },
+    { field: "productId", headerName: "Product ID", width: 150 },
+    { field: "manufacturerId", headerName: "Manufacturer ID", width: 150 },
+    { field: "retailerId", headerName: "Retailer ID", width: 150 },
     // { field: "status", headerName: "Status", width: 150 },
-    { field: "date", headerName: "Date", width: 150 },
+    { field: "date", headerName: "Deliverer Send Date", width: 200 },
     { field: "quantity", headerName: "Quantity", width: 150 },
 ];
