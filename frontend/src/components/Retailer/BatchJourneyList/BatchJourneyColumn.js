9c53259799147f1fd3bc69ce044ed66dadf66edf
MinhDinh-4869 <minhdeptrai4869@gmail.com>
2022-11-02 02:21:54 +0700

    "ok bro"

---
+++ frontend/src/components/Retailer/BatchJourneyList/BatchJourneyColumn.js
@@ -0,0 +1,10 @@
+export const BatchJourneyColumn = [
+    { field: "batchId", headerName: "Batch ID", width: 150 },
+    { field: "productId", headerName: "Product ID", width: 150,},
+    // { field: "manufacturerID", headerName: "Manufacturer ID", width: 100,},
+    { field: "manufacturerObj", headerName: "Manufacturer Name", width: 180,},
+    { field: "delivererObj", headerName: "Deliverer Name", width: 150,},
+    { field: "status", headerName: "Status", width: 180,},
+    // { field: "date", headerName: "Date", width: 10,},
+    { field: "quantity", headerName: "Quantity", width: 150,},
+];
---
+++ frontend/src/components/Retailer/BatchJourneyList/RetailerBatchJourney.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from 'react'
+import "./RetailerBatchJourney.css";
+import { DataGrid } from "@mui/x-data-grid";
+import { BatchJourneyColumn } from "./BatchJourneyColumn";
+// import TransferModal from './TransferModal';
+// import MarkFaultModal from './MarkFaultModal';
+import { request } from '../../../utils/request';
+import { Modal, Popover, Button, Timeline } from 'antd';
+
+
+function RetailerBatchJourney() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getBatch = async () => {
+      const token = localStorage.getItem("AUTH_DATA");
+      const params = {
+        method: "GET",
+        url: "/batch/all",
+        headers: { 'Content-Type': "application/json", 'x-access-token': token },
+      }
+      const response = await request(params);
+      let rawData = await response.text();
+      let jsonData = JSON.parse(rawData);
+      let body = jsonData["data"]; //take the body of data
+
+      body.forEach((element) => {
+        //component.date = component.date.orderedDate;
+        if (element.manufacturerObj) {
+          element.manufacturerObj = element["manufacturerObj"]["name"];
+        }
+        if (element.delivererObj) {
+          element.delivererObj = element["delivererObj"]["name"];
+        }
+      })
+
+      let newData = body.filter(component => component.status !== 'pending-registration');
+      setData(newData)
+    };
+    getBatch();
+    return () => {
+      // this now gets called when the component unmounts
+    };
+  }, []);
+
+  const [orderedDate, setOrderedDate] = useState("");
+  const [sendToDelivererDate, setSendToDelivererDate] = useState("");
+  const [sendToRetailerDate, setSendToRetailerDate] = useState("");
+  const [markedFaultDate, setMarkedFaultDate] = useState("");
+
+  let content = (
+    <div>
+      <Timeline>
+        <Timeline.Item color="gray">Ordered Date: {orderedDate}</Timeline.Item>
+        <Timeline.Item color="blue">Send To Deliverer Date: {sendToDelivererDate} </Timeline.Item>
+        <Timeline.Item color="green">Send To Retailer Date: {sendToRetailerDate}</Timeline.Item>
+        <Timeline.Item color="red">Marked Fault Date: {markedFaultDate}</Timeline.Item>
+      </Timeline>
+    </div>
+  );
+
+  const handleOnClickView = (a) => {
+    setOrderedDate(a.date.orderedDate);
+    setSendToDelivererDate(a.date.sendToDelivererDate);
+    setSendToRetailerDate(a.date.sendToRetailerDate);
+    setMarkedFaultDate(a.date.markedFaultDate);
+  }
+
+  // "markedFaultDate": "",
+  // "orderedDate": "2022-10-31",
+  // "sendToDelivererDate": "",
+  // "sendToRetailerDate": ""
+
+  const viewColumn = [
+    {
+      field: "viewaction",
+      headerName: "View",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellViewAction">
+            <Popover title="Date" content={content} trigger="click">
+              <Button type="primary" onClick={() => handleOnClickView(params.row)}>View</Button>
+            </Popover>
+          </div>
+        )
+      }
+    }
+  ];
+
+  return (
+    <>
+      {/* <TransferModal isTransfer={isTransfer} setIsTransfer={setIsTransfer} batchId={batchId} productId={productId} manufacturerId={manufacturerId} retailerId={retailerId} delivererId={delivererId} quantity={quantity} /> */}
+      
+      <div className="batchjourneylisttable">
+        <DataGrid
+          rows={data}
+          columns={BatchJourneyColumn.concat(viewColumn)}
+          getRowId={(row) => row.batchId}
+          pageSize={9}
+          rowsPerPageOptions={[9]}
+          checkboxSelection
+        />
+      </div>
+    </>
+
+  );
+};
+
+export default RetailerBatchJourney;
